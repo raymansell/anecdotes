b6566e6d43b9855ef0e00494145e112a1aa1f2df
raymansell <61888240+raymansell@users.noreply.github.com>
2021-03-14 21:36:31 -0500

    database connection

--- server/index.js
+++ server/index.js
@@ -1,3 +1,26 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import colors from 'colors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, console.log(`Server running on port: ${PORT}`.yellow.bold))
+  )
+  .catch((error) => console.log(error.message));
