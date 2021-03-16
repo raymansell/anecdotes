91de0feeed54e5a1e701733fdb88d7a389aeba1c
raymansell <61888240+raymansell@users.noreply.github.com>
2021-03-16 16:46:48 -0500

    anecdote submission form UI

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     <Container maxWidth='lg'>
       <AppBar className={classes.appBar} position='static' color='inherit'>
         <Typography className={classes.heading} variant='h2' align='center'>
-          Memories
+          Anecdotes
         </Typography>
         <img
           className={classes.image}
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,8 +1,117 @@
+import { useState } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import FileBase from 'react-file-base64';
+
 import useStyles from './styles';
 
 const Form = () => {
   const classes = useStyles();
-  return <div>Form</div>;
+
+  const [postData, setPostData] = useState({
+    creator: '',
+    title: '',
+    message: '',
+    tags: '',
+    selectedFile: '',
+  });
+  const handleSubmit = () => {};
+  const clear = () => {};
+
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete='off'
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant='h6'>Creating an Anecdote</Typography>
+        <TextField
+          name='creator'
+          variant='outlined'
+          label='Creator'
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData((prevState) => ({
+              ...prevState,
+              creator: e.target.value,
+            }))
+          }
+        ></TextField>
+        <TextField
+          name='title'
+          variant='outlined'
+          label='Title'
+          fullWidth
+          value={postData.titlee}
+          onChange={(e) =>
+            setPostData((prevState) => ({
+              ...prevState,
+              title: e.target.value,
+            }))
+          }
+        ></TextField>
+        <TextField
+          name='message'
+          variant='outlined'
+          label='Message'
+          fullWidth
+          value={postData.message}
+          onChange={(e) =>
+            setPostData((prevState) => ({
+              ...prevState,
+              message: e.target.value,
+            }))
+          }
+        ></TextField>
+        <TextField
+          name='tags'
+          variant='outlined'
+          label='Tags'
+          fullWidth
+          value={postData.tags}
+          onChange={(e) =>
+            setPostData((prevState) => ({
+              ...prevState,
+              tags: e.target.value,
+            }))
+          }
+        ></TextField>
+        <div className={classes.fileInput}>
+          <FileBase
+            type='file'
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData((prevState) => ({
+                ...prevState,
+                selectedFile: base64,
+              }))
+            }
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant='contained'
+          color='primary'
+          size='large'
+          type='submit'
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          size='small'
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
