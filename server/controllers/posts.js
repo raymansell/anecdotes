bb1f38eed99107322fae064d40061cec5ab3469f
raymansell <61888240+raymansell@users.noreply.github.com>
2021-03-18 21:18:14 -0500

    delete post route/controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -28,17 +28,28 @@ export const createPost = async (req, res) => {
 };
 
 export const updatePost = async (req, res) => {
-  const { id: _id } = req.params;
+  const { id } = req.params;
   const { title, message, creator, tags, selectedFile } = req.body;
 
-  if (!mongoose.Types.ObjectId.isValid(_id)) {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).send('No post with that id');
   }
 
   const post = { title, message, creator, tags, selectedFile };
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
     new: true,
     runValidators: true,
   });
   return res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send('No post with that id');
+  }
+
+  await PostMessage.findByIdAndRemove(id);
+  res.json({ message: 'Post deleted successfully' });
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { getPosts, createPost, updatePost } from '../controllers/posts.js';
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+} from '../controllers/posts.js';
 
 const router = express.Router();
 
 router.route('/').get(getPosts).post(createPost);
-router.route('/:id').patch(updatePost);
+router.route('/:id').patch(updatePost).delete(deletePost);
 
 export default router;
